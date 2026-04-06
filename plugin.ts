803f4df54a4707f8661ae84ebbc82421318d38bc
kagla <kagla@naver.com>
2026-04-06 21:14:35 +0900

    feat: myPageMenus 추가

--- plugin.ts
+++ plugin.ts
@@ -7,4 +7,9 @@ export default {
   repository: '',
   slug: 'shop',
   defaultEnabled: false,
+  myPageMenus: [
+    { label: '주문내역', icon: 'ClipboardList', subPath: '/mypage?tab=orders' },
+    { label: '찜 목록', icon: 'Heart', subPath: '/mypage?tab=wishlist' },
+    { label: '배송지', icon: 'MapPin', subPath: '/mypage?tab=addresses' },
+  ],
 }
